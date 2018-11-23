deff5db71afbb867b1b829d779fe0116de76c6c0
oscarhermawan <franky1790@gmail.com>
2018-11-23 16:44:22 +0800

    test-single-php

---
+++ single.php
@@ -0,0 +1,56 @@
+<?php
+  get_header();?>
+  <div class="row">
+    <div class="col-md-1"> </div>
+    <div class="col-md-10">
+      <!-- PACKAGES -->
+      <div class="row" id="tour-packages">
+        <div class="col-md-6">
+          <div class="box-for-padding">
+            <img src="http://cdnmedia.endeavorsuite.com/images/organizations/bc96f21d-28fb-46c2-bfb3-8e7292427224/inventory/2035437/176131_KTM-350-EXC-F-front-left-MY-2018-studio-e1494915765993.jpg" alt="1/2 Day Tabanan Jungle" width="100%"> <br/>
+            <div class="box-packages">
+              <i>From Rp. 23000</i>
+              <p>1/2 Day Tabanan Jungle<p>
+                <ul>
+                  <li> Located in Tabanan, beautiful city </li>
+                  <li> World Class Motorbike</li>
+                  <li> Nice Istructor will teach you</li>
+                  <li> Located in Tabanan, beautiful city</li>
+                  <li> World Class Motorbike</li>
+                  <li> Nice Istructor will teach you</li>
+                </ul>
+              <button type="button" class="btn btn-light">SEE DETAILS</button>
+              <button type="button" class="btn btn-danger">BOOK NOW</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="box-for-padding">
+            <img src="http://wallpaperlepi.com/wp-content/uploads/2014/09/KTM-RC8-Motor-Sport-Wallpaper.jpg" alt="1/2 Day Tabanan Jungle" width="100%"> <br/>
+            <div class="box-packages">
+              <i>From Rp. 23000</i>
+              <p>1/2 Day Tabanan Jungle<p>
+                <ul>
+                  <li> Located in Tabanan, beautiful city </li>
+                  <li> World Class Motorbike</li>
+                  <li> Nice Istructor will teach you</li>
+                  <li> Located in Tabanan, beautiful city</li>
+                  <li> World Class Motorbike</li>
+                  <li> Nice Istructor will teach you</li>
+                </ul>
+              <button type="button" class="btn btn-light">SEE DETAILS</button>
+              <button type="button" class="btn btn-danger">BOOK NOW</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- END PACKAGES -->
+
+    </div>
+    <div class="col-md-1"> </div> <!-- JUST FOR 1-10- AND 1 FOR CLOSE TAG GRID -->
+  </div>
+
+<?php
+  get_footer();
+?>
